12b4b88e272b676705402cfb92d2c68578882dfe
lqwsy <2567924198@qq.com>
2019-04-20 14:42:13 +0800

    first

---
+++ app/src/main/java/com/meteorshower/autoclock/service/ControllerAccessibilityService.java
@@ -0,0 +1,18 @@
+package com.meteorshower.autoclock.service;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class ControllerAccessibilityService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+    
+}
---
+++ app/src/main/java/com/meteorshower/autoclock/util/Utils.java
@@ -0,0 +1,4 @@
+package com.meteorshower.autoclock.util;
+
+public class Utils {
+}
