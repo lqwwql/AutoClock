4cb6c62a591f12b857f7cce469b66b02073480c1
lqw <2567924198@qq.com>
2023-08-29 23:50:02 +0800

    增加属性动画缩放，旋转

--- app/src/main/java/com/meteorshower/autoclock/activity/HomeActivity.java
+++ app/src/main/java/com/meteorshower/autoclock/activity/HomeActivity.java
@@ -1,9 +1,18 @@
 package com.meteorshower.autoclock.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
 import com.hjq.toast.Toaster;
 import com.hw.datepickerlibrary.util.CalendarUtil;
 import com.hw.datepickerlibrary.view.DatePopupWindow;
@@ -26,10 +35,14 @@ import butterknife.OnClick;
 public class HomeActivity extends BaseActivity {
 
     private LinearLayout llCalendar;
+    private LinearLayout llContent;
+    private ImageView ivArrow;
     private int startGroup = -1;//全局量
     private int endGroup = -1;
     private int startChild = -1;
     private int endChild = -1;
+    private boolean isRotate = false;
+    private boolean isScale = false;
 
     @Override
     protected int getLayoutID() {
@@ -44,6 +57,8 @@ public class HomeActivity extends BaseActivity {
             Toaster.show("无障碍服务已启动");
         }
         llCalendar = findViewById(R.id.ll_calendar);
+        llContent = findViewById(R.id.ll_content);
+        ivArrow = findViewById(R.id.iv_arrow);
     }
 
     @Override
@@ -66,7 +81,7 @@ public class HomeActivity extends BaseActivity {
 
     @OnClick({R.id.btn_start, R.id.btn_end, R.id.btn_add, R.id.btn_stop_running, R.id.btn_look,
             R.id.btn_test, R.id.btn_reset, R.id.btn_exc_cmd, R.id.btn_setting, R.id.btn_scroll_setting,
-            R.id.btn_test_view
+            R.id.btn_test_view, R.id.btn_test_view1, R.id.iv_arrow, R.id.btn_content
     })
     public void doClick(View view) {
         Log.d("lqwtest", "doClick id=" + view.getId());
@@ -117,14 +132,92 @@ public class HomeActivity extends BaseActivity {
                 startActivity(new Intent(HomeActivity.this, ScrollSettingActivity.class));
                 break;
             case R.id.btn_test_view:
-                showCalendar();
+                llContent.setPivotX(1.f);
+                llContent.setPivotY(0.f);
+                float transY = llContent.getHeight();
+                Log.d(AppConstant.TAG,"transY="+llContent.getHeight()+" "+llContent.getMeasuredHeight());
+//                ObjectAnimator translationY = ObjectAnimator.ofFloat(ivArrow, "translationY", 0f,transY);
+                ObjectAnimator animatorY = ObjectAnimator.ofFloat(llContent, "scaleY", 0f,1f);
+                ObjectAnimator animatorX = ObjectAnimator.ofFloat(llContent, "scaleX", 1f,1f);
+                AnimatorSet animationSet = new AnimatorSet();
+                animationSet.playTogether(animatorX,animatorY);
+                animationSet.setDuration(500);
+                animationSet.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        llContent.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+                animationSet.start();
+//                animator.setDuration(500);
+//                animator.start();
+                break;
+            case R.id.btn_test_view1:
+                llContent.setPivotX(1.f);
+                llContent.setPivotY(0.f);
+                float transY1 = llContent.getHeight();
+                Log.d(AppConstant.TAG,"transY1="+llContent.getHeight()+" "+llContent.getMeasuredHeight());
+//                ObjectAnimator translationY1 = ObjectAnimator.ofFloat(ivArrow, "translationY", 0f,-transY1);
+                ObjectAnimator animatorY1 = ObjectAnimator.ofFloat(llContent, "scaleY", 1f,0f);
+                ObjectAnimator animatorX1 = ObjectAnimator.ofFloat(llContent, "scaleX", 1f,1f);
+                AnimatorSet animationSet1 = new AnimatorSet();
+                animationSet1.playTogether(animatorX1,animatorY1);
+                animationSet1.setDuration(500);
+                animationSet1.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        llContent.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+                animationSet1.start();
+
+
+                break;
+            case R.id.iv_arrow:
+                Animation animation = AnimationUtils.loadAnimation(this, isRotate ? R.anim.rotate180to0 : R.anim.rotate0to180);
+                animation.setFillAfter(true);
+                ivArrow.startAnimation(animation);
+                isRotate = !isRotate;
+                break;
+            case R.id.btn_content:
+                Toaster.show("内容点击测试");
                 break;
             default:
                 break;
         }
     }
 
-    private void showCalendar(){
+    private void showCalendar() {
         new DatePopupWindow
                 .Builder(HomeActivity.this, Calendar.getInstance().getTime(), llCalendar)//初始化
                 .setInitSelect(startGroup, startChild, endGroup, endChild)//设置上一次选中的区间状态
@@ -140,7 +233,7 @@ public class HomeActivity extends BaseActivity {
                         endChild = endChildPosition;//结束对应月份中日的位置
                         String mStartTime = CalendarUtil.FormatDateYMD(startDate);
                         String mEndTime = CalendarUtil.FormatDateYMD(endDate);
-                        Log.d(AppConstant.TAG,"您选择了：" + mStartTime + "到" + mEndTime);
+                        Log.d(AppConstant.TAG, "您选择了：" + mStartTime + "到" + mEndTime);
                     }
                 }).builder();
     }
